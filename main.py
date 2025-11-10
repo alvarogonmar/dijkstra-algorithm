657d9a1621c019d6bb1fd3b86b9dcc67719ea085
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-09 19:14:18 -0600

    fix: instantiate Graph object correctly in main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from graph import Graph 
 
 if __name__ == "__main__":
-    graph_valle_real = Graph
-    graph_valle_real.add_edge('A', 'B', 257.5)
-    graph_valle_real.add_edge('A', 'C', 608.1)
-    graph_valle_real.add_edge('B', 'D', 292.8)
-    graph_valle_real.add_edge('D', 'F', 156.8)
-    graph_valle_real.add_edge('F', 'G', 186.7)
-    graph_valle_real.add_edge('G', 'H', 54.3)
-    graph_valle_real.add_edge('H', 'I', 80.8)
-    graph_valle_real.add_edge('I', 'C', 401.3)
-    graph_valle_real.add_edge('I', 'K', 136.9)
-    graph_valle_real.add_edge('K', 'L', 333.9)
-    graph_valle_real.add_edge('L', 'M', 260.7)
-    graph_valle_real.add_edge('M', 'Z', 158.6)
-    graph_valle_real.add_edge('F', 'J', 362.8)
-    graph_valle_real.add_edge('J', 'Z', 548.8)
+    graph_valle_real = Graph()
+    graph_valle_real.add_edge("A", "B", 257.5)
+    graph_valle_real.add_edge("A", "C", 608.1)
+    graph_valle_real.add_edge("B", "D", 292.8)
+    graph_valle_real.add_edge("D", "F", 156.8)
+    graph_valle_real.add_edge("F", "G", 186.7)
+    graph_valle_real.add_edge("G", "H", 54.3)
+    graph_valle_real.add_edge("H", "I", 80.8)
+    graph_valle_real.add_edge("I", "C", 401.3)
+    graph_valle_real.add_edge("I", "K", 136.9)
+    graph_valle_real.add_edge("K", "L", 333.9)
+    graph_valle_real.add_edge("L", "M", 260.7)
+    graph_valle_real.add_edge("M", "Z", 158.6)
+    graph_valle_real.add_edge("F", "J", 362.8)
+    graph_valle_real.add_edge("J", "Z", 548.8)
 
 
     # mostrar el grafo
     graph_valle_real.print_graph()
+
+    # Usar metodo dijkstra
+    start = "A"
+    print(f"Metodo Dijkstra desde {start}\n")
+
+    distances, previous = graph_valle_real.dijkstra(start)
