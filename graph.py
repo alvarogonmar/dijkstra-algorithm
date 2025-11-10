2399434a48da2f2381ec74a68b35c5f967dc1699
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-09 18:24:47 -0600

    add dijkstra method for shortest path calculation in Graph class

--- graph.py
+++ graph.py
@@ -1,5 +1,3 @@
-import  heapq
-
 class Graph:
     def __init__(self):
         self.graph = {}
@@ -12,3 +10,14 @@ class Graph:
         
         self.graph[u].append((v, weight))
         self.graph[v].append((u, weight))
+
+    def dijkstra(self, start):
+        distances = {node: float('inf') for node in self.graph} # Diccionario con las distancias mas cortas
+        distances[start] = 0
+
+        visited = [] # Array para guardar los nodos que ya visitamos
+
+        previous_path =  {node: None for node in self.graph} # Guardar el camino anterior
+
+        while len(visited) < len(self.graph): # Mientras no hemos visitado todos los nodos
+            
